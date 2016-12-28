055a04cafa6cbc984b751ad359479030877162f7
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2016-12-28 18:00:56 +0800

    [EDIT] create proposal form has been added (incomplete) - added programme schedule, equipment borrowing and budget section. [ADD] tooltip styling

--- resources/views/proposal/create.blade.php
+++ resources/views/proposal/create.blade.php
@@ -91,7 +91,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="date" class="col-sm-2 control-label">ATTENDANCE <small><a data-toggle="tooltip" data-placement="top" title="Rough estimate numbers of event attendees">?</a></small></label>
+                            <label for="date" class="col-sm-2 control-label">ATTENDANCE <small><a class="tooltip" data-toggle="tooltip" data-placement="top" title="Rough estimate numbers of event attendees">?</a></small></label>
                             <div class="col-sm-10">
                               <input type="number" class="form-control datepicker" id="eventEstimatedAttendance">
                             </div>
@@ -124,7 +124,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="objective" class="col-sm-2 control-label">OC NAME <small><a data-toggle="tooltip" data-placement="top" title="Event Organiser / Organising Chairperson">?</a></small></label></label>
+                            <label for="objective" class="col-sm-2 control-label">OC NAME <small><a class="tooltip" data-toggle="tooltip" data-placement="top" title="Event Organiser / Organising Chairperson">?</a></small></label></label>
                             <div class="col-sm-10">
                               <input type="text" class="form-control" id="organisername">
                             </div>
@@ -147,9 +147,10 @@
                         {{-- Button to next part --}}
                         <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-10">
-                              <a class="btn btn-primary" href="#event-programme" aria-controls="home" role="tab" data-toggle="tab">Next</a>
+                                <a class="btn btn-primary" href="#event-programme" aria-controls="event-programme" role="tab" data-toggle="tab">Next <span class="glyphicon glyphicon-chevron-right"></span></a>
                             </div>
                         </div>
+
                     </div> {{-- end .form-horizontal --}}
                 </div> {{-- end tab: 1. Event Description --}}
 
@@ -159,6 +160,41 @@
                             <h4 class="col-sm-12 text-center">EVENT PROGRAMME</h4>
                         </div>
 
+                        <div class="table-responsive">
+                            <table class="table table-striped table-condensed" id="event_programme_table">
+                                <tr>
+                                    <td class="text-center">Date/Time</td>
+                                    <td class="text-center">Schedule</td>
+                                    <td class="text-center">Venue</td>
+                                    <td></td>
+                                </tr>
+                                <tr class="hidden tr_template">
+                                    <td class="datetime">
+                                        <input type="text" class="form-control">
+                                    </td>
+                                    <td class="schedule">
+                                        <input type="text" class="form-control">
+                                    </td>
+                                    <td class="venue">
+                                        <input type="text" class="form-control">
+                                    </td>
+                                    <td class="remove">
+                                        <button class="btn btn-danger pull-right"><span class="glyphicon glyphicon-remove"></span></button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="4">
+                                        <button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> ADD TIMESLOT</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+
+                        {{-- Button to next part --}}
+                        <a class="btn btn-default" href="#event-description" aria-controls="event-description" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-chevron-left"></span> Previous</a>
+
+                        <a class="btn btn-primary" href="#event-equipment" aria-controls="event-equipment" role="tab" data-toggle="tab">Next <span class="glyphicon glyphicon-chevron-right"></span></a>
+
                     </div> {{-- end .form-horizontal --}}
                 </div> {{-- end tab: 2. Event Programme --}}
 
@@ -167,6 +203,42 @@
                         <div class="form-group">
                             <h4 class="col-sm-12 text-center">BORROW EQUIPMENT FROM FACILITIES/ITS</h4>
                         </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped" id="borrow_equipments_table">
+                                <tr>
+                                    <td class="text-center col-md-9">Item <a class="tooltip" data-toggle="tooltip" data-placement="top" title="e.g: Microphones, Signage stands, extension wires, etc...">?</a>
+                                    </td>
+                                    <td class="text-center col-md-2">Amount</td>
+                                    <td class="col-md-1"></td>
+                                </tr>
+                                <tr class="hidden tr_template">
+                                    <td class="item">
+                                        <select class="form-control item_select">
+                                            <option value="item001">Microphone</option>
+                                            <option value="item002">Signage stand</option>
+                                            <option value="item003">Extension wire (5M)</option>
+                                            <option value="item004">Extension wire (15M)</option>
+                                        </select>
+                                    </td>
+                                    <td class="amount">
+                                        <input type="number" class="form-control">
+                                    </td>
+                                    <td class="remove">
+                                        <button class="btn btn-danger pull-right"><span class="glyphicon glyphicon-remove"></span></button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> ADD ITEM</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+
+                        {{-- Button to next part --}}
+                        <a class="btn btn-default" href="#event-programme" aria-controls="event-programme" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-chevron-left"></span> Previous</a>
+
+                        <a class="btn btn-primary" href="#event-budget" aria-controls="event-budget" role="tab" data-toggle="tab">Next <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                     </div> {{-- end .form-horizontal --}}
                 </div> {{-- end tab: 3. Event Equipment --}}
@@ -176,6 +248,130 @@
                         <div class="form-group">
                             <h4 class="col-sm-12 text-center">CASH FLOW</h4>
                         </div>
+
+                        <div class="form-group">
+                            <h5 class="col-sm-12">EXPECTED EXPENDITURE</h5>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered" id="budget_expenditure_table">
+                                <tr>
+                                    <td class="text-center col-md-9">Description</td>
+                                    <td class="text-center col-md-2">Cost</td>
+                                    <td class="col-md-1"></td>
+                                </tr>
+                                <tr class="hidden tr_template">
+                                    <td class="description">
+                                        <input type="text" class="form-control">
+                                    </td>
+                                    <td class="cost">
+                                        <div class="input-group">
+                                            <div class="input-group-addon">RM</div>
+                                            <input type="number" class="form-control">
+                                        </div>
+                                    </td>
+                                    <td class="remove">
+                                        <button class="btn btn-danger pull-right"><span class="glyphicon glyphicon-remove"></span></button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> ADD ITEM</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div> {{-- end .table-responsive --}}
+                        <div class="form-group">
+                            <label class="col-sm-8 control-label">TOTAL EXPENDITURE</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" value="0" disabled>
+                            </div>
+                        </div> {{-- end total expenditure --}}
+                        {{-- end #budget_expenditure_table --}}
+
+                        <div class="form-group">
+                            <h5 class="col-sm-12">EXPECTED INCOME</h5>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered" id="budget_income_table">
+                                <tr>
+                                    <td class="text-center col-md-9">Description</td>
+                                    <td class="text-center col-md-2">Cost</td>
+                                    <td class="col-md-1"></td>
+                                </tr>
+                                <tr class="hidden tr_template">
+                                    <td class="description">
+                                        <input type="text" class="form-control">
+                                    </td>
+                                    <td class="cost">
+                                        <div class="input-group">
+                                            <div class="input-group-addon">RM</div>
+                                            <input type="number" class="form-control">
+                                        </div>
+                                    </td>
+                                    <td class="remove">
+                                        <button class="btn btn-danger pull-right"><span class="glyphicon glyphicon-remove"></span></button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> ADD ITEM</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div> {{-- end .table-responsive --}}
+                        <div class="form-group">
+                            <label class="col-sm-8 control-label">TOTAL INCOME</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" value="0" disabled>
+                            </div>
+                        </div> {{-- end total income --}}
+                        {{-- end #budget_expenditure_table --}}
+
+                        <div class="form-group">
+                            <h5 class="col-sm-12">FUNDS REQUESTED FROM SSSC</h5>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered" id="budget_expenditure_table">
+                                <tr>
+                                    <td class="text-center col-md-9">Description</td>
+                                    <td class="text-center col-md-2">Cost</td>
+                                    <td class="col-md-1"></td>
+                                </tr>
+                                <tr class="hidden tr_template">
+                                    <td class="description">
+                                        <input type="text" class="form-control">
+                                    </td>
+                                    <td class="cost">
+                                        <div class="input-group">
+                                            <div class="input-group-addon">RM</div>
+                                            <input type="number" class="form-control">
+                                        </div>
+                                    </td>
+                                    <td class="remove">
+                                        <button class="btn btn-danger pull-right"><span class="glyphicon glyphicon-remove"></span></button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                        <button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> ADD ITEM</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div> {{-- end .table-responsive --}}
+                        <div class="form-group">
+                            <label class="col-sm-8 control-label">APPROVED BY SSSC</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" value="0" disabled>
+                            </div>
+                        </div> {{-- end total cost --}}
+                        {{-- end #budget_expenditure_table --}}
+
+                        {{-- Button to next part --}}
+                        <a class="btn btn-default" href="#event-equipment" aria-controls="event-equipmenet" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-chevron-left"></span> Previous</a>
+
+                        <a class="btn btn-primary" href="#event-responseplan" aria-controls="event-responseplan" role="tab" data-toggle="tab">Next <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                        <button type="submit" class="btn btn-success submit" data-method="POST" data-url="{{ route('club.store') }}">Submit</button>
 
                     </div> {{-- end .form-horizontal --}}
                 </div> {{-- end tab: 4. Event Budget --}}
